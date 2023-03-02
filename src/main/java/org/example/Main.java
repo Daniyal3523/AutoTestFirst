73f38b7b4caef483ed4e7df8001b46db177ba74a
Muhammad Daniyal <muhammaddaniyal@Acunas-MacBook-Pro.local>
2023-03-02 16:10:11 -0500

    added .gitignore file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -145,6 +145,7 @@ public class Main {
             driver.findElement(By.id("opentab")).isDisplayed();
             Thread.sleep(2000);
         }
+        System.out.println("Hello World");
 
     }
 
