a198a5e9d764546fdd380a4bedb14801b9e3ec77
Muhammad Daniyal <muhammaddaniyal@Acunas-MacBook-Pro.local>
2023-03-02 15:13:59 -0500

    new stuff

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -36,7 +43,6 @@ public class Main {
        WebDriverManager.chromedriver().setup();
        driver = new ChromeDriver();
        driver.get("https:www.facebook.com");
-
     }
 
     @Test
@@ -82,14 +88,67 @@ public class Main {
        String errorMessage = driver.findElement(By.cssSelector("#quote-error-alert")).getText();
         System.out.println(errorMessage); //-> Not to use in professional world
         Assert.assertEquals(errorMessage, expectedZipCodeErrorMessage);
-
-
-
     }
-    @AfterTest
+    /*@AfterTest
     public void tearDown() {
        driver.quit();
+    }*/
+
+    @Test
+    public void validateLoginPage() {
+       WebDriverManager.chromedriver().setup();
+       driver = new ChromeDriver();
+       driver.get("https://www.statefarm.com");
+       driver.findElement(By.xpath("//*[@id='quote-main-zip-code-input']")).sendKeys("07508");
+       driver.findElement(By.id("quote-main-zip-btn")).click();
     }
+    @Test
+    public void testDropDown() {
+        List<String> expectedDropDown = new ArrayList<>(Arrays.asList("Select", "Option1", "Option2", "Option3"));
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        Select dropDownList = new Select(driver.findElement(By.id("dropdown-class-example")));
+        dropDownList.selectByVisibleText("Option1");
+        List<WebElement> e = dropDownList.getOptions();
+        for (int i = 0; i<e.size(); i++) {
+            Assert.assertEquals(e.get(i).getText(),expectedDropDown.get(i));
+        }
+    }
+    @Test
+    public void aletExample() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.findElement(By.id("alertbtn")).click();
+        driver.switchTo().alert().accept();
+        Thread.sleep(3000);
+    }
+    @Test
+    public void swithTab() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        String mainWindow = driver.getWindowHandle();
+        driver.findElement(By.id("opentab")).click();
+        Thread.sleep(2300);
+        Set<String> wins = driver.getWindowHandles();
+        for (String w : wins) {
+            if (!w.equalsIgnoreCase(mainWindow)) {
+                driver.switchTo().window(w);
+                Thread.sleep(2000);
+                String courses = driver.findElement(By.linkText("Courses")).getText();
+                Assert.assertEquals(courses, "Courses");
+                driver.close();
+            }
+            driver.switchTo().window(mainWindow);
+            driver.findElement(By.id("opentab")).isDisplayed();
+            Thread.sleep(2000);
+        }
+
+    }
+
+
 
 
 }
